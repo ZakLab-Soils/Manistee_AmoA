2bb639cdad482b186edbf15f3c398b513975ef1b
ZakLab-Soils <rimaup@umich.edu>
2022-04-09 22:19:01 -0400

    Update stackedbarplot.r

--- code/stackedbarplot.r
+++ code/stackedbarplot.r
@@ -24,5 +24,11 @@ TAX2.stand.mean$STAND <- gsub("Stand_*", "", fixed=FALSE, TAX2.stand.mean$STAND)
 TAX2.stand.mean <- TAX2.stand.mean[order(TAX2.stand.mean$STAND), ]
 
 TAX2.colors <- c("purple", "mediumorchid2", "mediumpurple2", "purple3", "lavender", "navy")
-
+TAX2.stand.mean$STAND <- factor(TAX2.stand.mean$STAND, levels = c("58", "7", "6", "41", "24", "100", "22"))
 ggplot(TAX2.stand.mean, aes(fill=TAX2, y=PROP, x=STAND)) + geom_bar(position = "fill", stat = "identity") + ylab(paste0("Relative Abundance")) + xlab(paste0("Stand")) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), panel.background = element_blank()) +scale_fill_manual(values = TAX2.colors)
+
+#Removed NS-OTU1 since <0.05% of data
+TAX2.stand.mean.nonsout1 <- aggregate(PROP~TAX2+STAND, TAX2.all.tmp.tbl[TAX2.all.tmp.tbl$TAX2 != "NS_OTU1",], mean)
+TAX2.stand.mean.nonsout1$STAND <- gsub("Stand_*", "", fixed=FALSE, TAX2.stand.mean.nonsout1$STAND)
+TAX2.stand.mean.nonsout1$STAND <- factor(TAX2.stand.mean.nonsout1$STAND, levels = c("58", "7", "6", "41", "24", "100", "22"))
+ggplot(TAX2.stand.mean.nonsout1, aes(fill=TAX2, y=PROP, x=STAND)) + geom_bar(position = "fill", stat = "identity") + ylab(paste0("Relative Abundance")) + xlab(paste0("Stand")) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), panel.background = element_blank()) +scale_fill_manual(values = TAX2.colors.nonsotu1)
