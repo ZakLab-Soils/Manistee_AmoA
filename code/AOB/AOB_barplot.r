14b4664bc091818cf5bad98716898bf7aa8ac597
ZakLab-Soils <rimaup@umich.edu>
2023-04-13 15:57:35 -0400

    Create AOB_barplot.r

---
+++ code/AOB/AOB_barplot.r
@@ -0,0 +1,51 @@
+#Create and analyze barplot for Cluster groupings
+library(tidyverse)
+library(ggplot2)
+
+
+phy.aob.pruned <- readRDS("Phyloseq_AOB_Pruned.rds")
+
+Cl.all.aob.tbl <- readRDS("CL_ALL_AOB_Table.rds)
+
+CL1.aob.tbl <-  Cl.all.aob.tbl %>% group_by(CL1, STAND, PLOT) %>% summarize(COUNT = sum(COUNT))%>% ungroup()%>% select(CL1, STAND, PLOT, COUNT)
+
+CL1.aob.tmp.tbl <- CL1.aob.tbl  %>% group_by(CL1) %>% mutate(CL1.TOTAL = sum(COUNT))%>% ungroup() %>% filter(CL1.TOTAL > 0) %>% group_by(PLOT) %>% mutate(PLOT.COUNT = sum(COUNT))%>% ungroup() %>% group_by(CL1, PLOT) %>% mutate(PROP = sum(COUNT)/PLOT.COUNT) %>% ungroup() %>% mutate(HELLINGER = sqrt(PROP))
+
+#At the 0.1 pairwise clustering, all AOB ASVs collapse into 6 clusters
+CL1.stand.aob.mean <- aggregate(PROP~CL1+STAND, CL1.aob.tmp.tbl, mean)
+CL1.stand.aob.mean$STAND <- gsub("Stand_*", "", fixed=FALSE, CL1.stand.aob.mean$STAND)
+CL1.stand.aob.mean <- CL1.stand.aob.mean[order(CL1.stand.aob.mean$STAND), ]
+CL1.stand.aob.mean$STAND <- factor(CL1.stand.aob.mean$STAND, levels = c("58", "9", "7", "41", "100", "6", "24", "22"))
+
+CL1.aob.colors <- c("purple", "mediumorchid2" , "mediumpurple", "purple4", "navy", "lavender")
+CL1.stand.aob.mean.plot <- ggplot(CL1.stand.aob.mean, aes(fill=factor(CL1), y=PROP, x=STAND)) + geom_bar(position = "fill", stat = "identity") + ylab(paste0("Relative Abundance")) + xlab(paste0("Stand")) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.border = element_blank(), panel.background = element_blank()) +scale_fill_manual(values = CL1.aob.colors) + guides(fill=guide_legend(title="Cluster\n0.1"))
+
+ggsave("Barplot_Cluster_P1_AOB.pdf", width = 8.5, height = 10)
+
+###STILL EDITING
+#Sideways barplot with TAX2 Clades separated out
+boxplot.colors.aoa <- c("coral4", "coral3", "pink2", "purple", "dodgerblue2", "dodgerblue4", "darkblue")
+
+#Test for significance for each Clade with Kruskal test and Pairwise-Wilcox
+TAX2.plot.stand.aoa.mean <- aggregate(PROP~TAX2+PLOT+STAND, TAX2.aoa.tmp.tbl, mean)
+
+tax2.nsbeta.aoa <- TAX2.plot.stand.aoa.mean[TAX2.plot.stand.aoa.mean$TAX2 == "NS-Beta",]
+tax2.nsdelta.aoa <- TAX2.plot.stand.aoa.mean[TAX2.plot.stand.aoa.mean$TAX2 == "NS-Delta",]
+tax2.nsalpha.aoa <- TAX2.plot.stand.aoa.mean[TAX2.plot.stand.aoa.mean$TAX2 == "NS-Alpha",]
+tax2.nsgamma.aoa <- TAX2.plot.stand.aoa.mean[TAX2.plot.stand.aoa.mean$TAX2 == "NS-Gamma",]
+tax2.ntalpha.aoa <- TAX2.plot.stand.aoa.mean[TAX2.plot.stand.aoa.mean$TAX2 == "NT-Alpha",]
+
+ktnsa.aoa <- kruskal.test(PROP~STAND, data = tax2.nsalpha.aoa)
+ktnta.aoa <- kruskal.test(PROP~STAND, data = tax2.ntalpha.aoa)
+ktnsg.aoa <- kruskal.test(PROP~STAND, data = tax2.nsgamma.aoa)
+ktnsd.aoa <- kruskal.test(PROP~STAND, data = tax2.nsdelta.aoa)
+ktnsb.aoa <- kruskal.test(PROP~STAND, data = tax2.nsbeta.aoa)
+
+ptnsa.aoa <- pairwise.wilcox.test(tax2.nsalpha.aoa$PROP, g=tax2.nsalpha.aoa$STAND, p.adjust.method = "BH")
+ptnsg.aoa  <- pairwise.wilcox.test(tax2.nsgamma.aoa$PROP, g=tax2.nsgamma.aoa$STAND, p.adjust.method = "BH")
+ptnsd.aoa  <- pairwise.wilcox.test(tax2.nsdelta.aoa$PROP, g=tax2.nsdelta.aoa$STAND, p.adjust.method = "BH")
+ptnsb.aoa  <- pairwise.wilcox.test(tax2.nsbeta.aoa$PROP, g=tax2.nsbeta.aoa$STAND, p.adjust.method = "BH")
+ptnta.aoa  <- pairwise.wilcox.test(tax2.ntalpha.aoa$PROP, g=tax2.ntalpha.aoa$STAND, p.adjust.method = "BH")
+
+myresults <- list(ktnsa.aoa, ktnsb.aoa, ktnsd.aoa, ktnsg.aoa, ktnta.aoa, ptnsa.aoa, ptnsb.aoa, ptnsd.aoa, ptnsg.aoa, ptnta.aoa)
+capture.output(myresults, file = "Kruskal_Wilcox_Results_Cluster1.txt")
