0bf374e9552d51d3a6d958d27a54a4f3887e7eb8
ZakLab-Soils <rimaup@umich.edu>
2023-11-30 17:34:20 -0500

    Update AOB1_dada2.r

--- code/AOB/AOB1_dada2.r
+++ code/AOB/AOB1_dada2.r
@@ -2,8 +2,6 @@ library(dada2)
 library(ShortRead)
 library(Biostrings)
 
-#setwd for folder where plan to sort and store output for R ("C:/**/**/amoa-AOB/")
-
 set.seed(496571)
 
 path <- "../../reads/" #This folder contains the sequence data - can be same as wd if the reads are in that folder
@@ -15,10 +13,8 @@ fnRs <- sort(list.files(path, pattern = "_R2_001.fastq.gz", full.names = TRUE))
 #Check quality of reads - this step takes a long time and may need to be broken down into sections.
 plotQualityProfile(fnFs)
 plotQualityProfile(fnRs)
-#plotQualityProfile(fnFs[11:14])
 
 ##Primer removal and checking for primer contamination
-
 fnFs.filtN <- file.path(path, "filtN", basename(fnFs))
 fnRs.filtN <- file.path(path, "filtN", basename(fnRs))
  
@@ -45,7 +41,6 @@ for(i in seq_along(fnFs)){system2(cutadapt, args = c(R1.flags, R2.flags, "-n", 2
 #Cutadapt Command line parameters: -g ATGGTCTGGCTWAGACG -a TGGACATACAGATGGATGGC -G GCCATCCATCTGTATGTCCA -A CGTCTWAGCCAGACCAT -n 2 -o data/cutadapt_processed/AOA7_S1_L001_R1_001.fastq.gz #-p data/cutadapt_processed/AOA7_S1_L001_R2_001.fastq.gz data/filtN/AOA7_S1_L001_R1_001.fastq.gz data/filtN/AOA7_S1_L001_R2_001.fastq.gz
 
 #See how much the primers appear in the data for selected libraries. To check all libraries change[N] to different library numbers.
-
 allOrients <- function(primer) {
 require(Biostrings)
 dna <- DNAString(primer)
@@ -63,7 +58,6 @@ return(sum(nhits > 0))
 }
 
 rbind(FWD.FowardReads = sapply(FWD.orients, primerHits, fn=fnFs.filtN[[1]]), FWD.ReverseReads = sapply(FWD.orients, primerHits, fn=fnRs.filtN[[1]]), REV.ForwardReads = sapply(REV.orients, primerHits, fn=fnFs.filtN[[1]]), REV.ReverseReads = sapply(REV.orients, primerHits, fn=fnRs.filtN[[1]]))
-
 #                Forward Complement Reverse RevComp
 #FWD.FowardReads        x          x       x       x
 #FWD.ReverseReads       x          x       x       x
@@ -81,7 +75,6 @@ cutFs <- sort(list.files(path.cut, pattern = "R1_001.fastq.gz", full.names = TRU
 cutRs <- sort(list.files(path.cut, pattern = "R2_001.fastq.gz", full.names = TRUE))
 
 ## Filter and trim for length, quality and size; Calculate error rate.
-
 get.sample.name <- function(fname) strsplit(basename(fname), "-|_" )[[1]][3]
 sample.names <- unname(sapply(cutFs, get.sample.name))
 head(sample.names)
@@ -89,17 +82,12 @@ head(sample.names)
 filtFs <- file.path(path.cut, "filtered", basename(cutFs))
 filtRs <- file.path(path.cut, "filtered", basename(cutRs))
 
-
-#Still re-editing
 filter.summary <- filterAndTrim(cutFs, filtFs, cutRs, filtRs, maxN = 0, truncLen = c(245,220), minLen = c(200,200), maxEE = c(2,2), truncQ = 2, rm.phix = TRUE, compress = TRUE, multithread = FALSE)
-#filter.summary <- filterAndTrim(cutFs, filtFs, cutRs, filtRs, maxN = 0, maxEE = c(2,2), truncQ = 2, minLen = c(200,200), rm.phix = TRUE, compress = TRUE, multithread = FALSE)
 
-#Double check the quality looks acceptable across reads. We felt it was not not necessary to trim the read lengths for this data set.
 plotQualityProfile(filtFs)
 plotQualityProfile(filtRs)
 
 ##Error rate processing, dereplication and ASV inference and sequence table formation
-
 errF <- learnErrors(filtFs, multithread = FALSE)
 errR <- learnErrors(filtRs, multithread = FALSE)
 
@@ -124,7 +112,7 @@ seqtab <- makeSequenceTable(mergers)
 seqtab.452 <- seqtab[,nchar(colnames(seqtab)) %in% 451:453]
 saveRDS(seqtab.452, file="Seqtab_452_AOB.rds")
 
-seqtab.nochim <- removeBimeraDenovo(seqtab.multi, method = "pooled", multithread = FALSE, verbose = TRUE)
+seqtab.nochim <- removeBimeraDenovo(seqtab.452, method = "pooled", multithread = FALSE, verbose = TRUE)
 dim(seqtab.nochim)
 sum(seqtab.nochim)/sum(seqtab)
 
@@ -154,4 +142,3 @@ library(genefilter)
 flist <- filterfun_sample(kOverA(2, 1))
 phy.aob.pruned <- filter_taxa(phy.aob, flist, TRUE)
 saveRDS(phy.aob.pruned, file = "Phyloseq_AOB_Pruned.rds")
-
